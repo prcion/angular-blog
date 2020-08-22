530e7f8961a388300dae9bfa215b0ca12643f023
Ion <prc_ion@outlook.com>
2020-08-22 21:32:38 +0300

    add login form 8

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  private form: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
+  public getForm(): FormGroup {
+    return this.form;
+  }
+
+  public submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+
+
+  }
+
+  public isNotTouched(value: string): boolean {
+    return this.form.get(value).touched;
+  }
+
+  public isInvalid(value: string): boolean {
+    return this.form.get(value).invalid;
+  }
+
+  public hasErrorsRequired(value: string): boolean {
+    return this.form.get(value).errors.required;
+  }
+
+  public invalidEmail(): boolean {
+    return this.form.get('email').errors.mail;
+  }
+
+  public isNotTouchedAndInvalid(value: string): boolean {
+    return this.isNotTouched(value) && this.isInvalid(value);
+  }
+
+  public invalidPassword(): boolean {
+    return this.form.get('password').errors.minlength;
+  }
 }
