1e3b8344ef1e9a2181f1c13bacc2afa6a5ee85e1
Ion <prc_ion@outlook.com>
2020-09-05 19:10:35 +0300

    19

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../../shared/posts.service';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Array<Post>;
+  pSub: Subscription;
+  searchPosts: '';
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
+  remove(id: string): void {
+  }
 }
